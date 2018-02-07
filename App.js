15b1279cc43374e42d6bb5c78fea10085b48c3c9
lill <dalintorres@icloud.com>
2018-02-07 16:22:01 +0800

    add tab-navigator

--- App.js
+++ App.js
@@ -6,20 +6,52 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    Image,
+    Platform,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
-import SplashScreen from 'react-native-splash-screen'
-
+import imageSource from './src/img';
+import SplashScreen from 'react-native-splash-screen';
+import TabNavigator from 'react-native-tab-navigator';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
+    ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
+    android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
+
+class Home extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!--dalin
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit App.js
+                </Text>
+                <Text style={styles.instructions}>
+                    {instructions}
+                </Text>
+            </View>
+        )
+    }
+}
+
+class Friend extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Profile
+                </Text>
+            </View>
+        )
+    }
+}
 
 export default class App extends Component<{}> {
     componentDidMount() {
@@ -28,39 +60,80 @@ export default class App extends Component<{}> {
         SplashScreen.hide();
     }
 
-    render() {
+    state = {
+        selectedTab: 'home'
+    };
 
+    render() {
         return (
-            <View style={styles.container}>
-              <Text style={styles.welcome}>
-                Welcome to React Native!--dalin
-              </Text>
-              <Text style={styles.instructions}>
-                To get started, edit App.js
-              </Text>
-              <Text style={styles.instructions}>
-                  {instructions}
-              </Text>
-            </View>
+            <TabNavigator style={styles.container}>
+                <TabNavigator.Item
+                    selected={this.state.selectedTab === 'home'}
+                    title="对话"
+                    selectedTitleStyle={styles.selectedTitleStyle}
+                    renderIcon={() => <Image source={imageSource.home}/>}
+                    renderSelectedIcon={() => <Image source={imageSource.home_selected}/>}
+                    // badgeText="99" //TODO
+                    onPress={() => this.setState({selectedTab: 'home'})}>
+                    <Home/>
+                </TabNavigator.Item>
+                <TabNavigator.Item
+                    selected={this.state.selectedTab === 'friend'}
+                    title="密友"
+                    selectedTitleStyle={styles.selectedTitleStyle}
+                    titleStyle={styles.titleStyle}
+                    renderIcon={() => <Image source={imageSource.friend}/>}
+                    renderSelectedIcon={() => <Image source={imageSource.friend_selected}/>}
+                    onPress={() => this.setState({selectedTab: 'friend'})}>
+                    <Friend/>
+                </TabNavigator.Item>
+                <TabNavigator.Item
+                    selected={this.state.selectedTab === 'find'}
+                    title="发现"
+                    selectedTitleStyle={styles.selectedTitleStyle}
+                    titleStyle={styles.titleStyle}
+                    renderIcon={() => <Image source={imageSource.find}/>}
+                    renderSelectedIcon={() => <Image source={imageSource.find_selected}/>}
+                    onPress={() => this.setState({selectedTab: 'find'})}>
+                    <Home/>
+                </TabNavigator.Item>
+                <TabNavigator.Item
+                    selected={this.state.selectedTab === 'me'}
+                    title="我"
+                    selectedTitleStyle={styles.selectedTitleStyle}
+                    titleStyle={styles.titleStyle}
+                    renderIcon={() => <Image source={imageSource.me}/>}
+                    renderSelectedIcon={() => <Image source={imageSource.me_selected}/>}
+                    onPress={() => this.setState({selectedTab: 'me'})}>
+                    <Friend/>
+                </TabNavigator.Item>
+            </TabNavigator>
         );
     }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+    titleStyle: {
+        fontSize: 10,
+        marginBottom: 5,
+    },
+    selectedTitleStyle: {
+        color: '#44A1FD'
+    },
 });
---
+++ src/img/index.js
@@ -0,0 +1,12 @@
+const imageSource = {
+    home: require('./icon/home_selected.png'),
+    home_selected: require('./icon/home_selected.png'),
+    friend: require('./icon/home_selected.png'),
+    friend_selected: require('./icon/home_selected.png'),
+    find: require('./icon/home_selected.png'),
+    find_selected: require('./icon/home_selected.png'),
+    me: require('./icon/home_selected.png'),
+    me_selected: require('./icon/home_selected.png')
+};
+
+export default imageSource;
